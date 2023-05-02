f3b971dcf4bc0d03d4f3ec2620dd9485b5ec290d
Caio Barbieri <cbarbieri@vertigo.com.br>
2023-05-02 13:47:40 -0300

    Update S3 bucket object resource to use for_each map and regex function

--- terraform/cloudfront/website.tf
+++ terraform/cloudfront/website.tf
@@ -19,13 +19,13 @@ locals {
 }
 
 resource "aws_s3_bucket_object" "all_files" {
-  for_each = fileset("website/src/", "**/*")
+  for_each = { for f in fileset("website/src/", "**/*") : f => f }
 
   bucket        = aws_s3_bucket.frontend.bucket
-  key           = each.value
-  source        = "website/src/${each.value}"
-  etag          = filemd5("website/src/${each.value}")
-  content_type  = lookup(local.mime_types, replace(each.value, "/.*\\.(.+)$/", "$1"), "binary/octet-stream")
+  key           = each.key
+  source        = "website/src/${each.key}"
+  etag          = filemd5("website/src/${each.key}")
+  content_type  = lookup(local.mime_types, regex(".*\\.(.+)$", each.key), "binary/octet-stream")
 }
 
 output "all_files" {
