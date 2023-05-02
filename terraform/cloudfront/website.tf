7b7688cad87891368869a73b90cf3e0f2c697d8b
Caio Barbieri <cbarbieri@vertigo.com.br>
2023-05-02 14:08:25 -0300

    Update S3 bucket object resource with try

--- terraform/cloudfront/website.tf
+++ terraform/cloudfront/website.tf
@@ -25,7 +25,7 @@ resource "aws_s3_bucket_object" "all_files" {
   key           = each.value
   source        = "website/src/${each.value}"
   etag          = filemd5("website/src/${each.value}")
-  content_type  = lookup(local.mime_types, matchkeys(keys(local.mime_types), keys(local.mime_types), [regex(".*\\.(.+)$", each.value)[0]])[0], "binary/octet-stream")
+  content_type  = try(local.mime_types[regex(".*\\.(.+)$", each.value)[0]], "binary/octet-stream")
 }
 
 output "all_files" {
