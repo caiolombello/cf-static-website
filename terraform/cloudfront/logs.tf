7ccb066dba9b5be6853d89d7753ddfc3552b27b4
Caio Barbieri <cbarbieri@vertigo.com.br>
2023-05-13 15:00:50 -0300

    fix statement

--- terraform/cloudfront/logs.tf
+++ terraform/cloudfront/logs.tf
@@ -28,6 +28,7 @@ resource "aws_s3_bucket_policy" "allow_access_from_cloudfront" {
       "Principal": {
         "AWS": "${aws_cloudfront_origin_access_identity.frontend_origin_access_identity.iam_arn}"
       }
+    }
   ]
 }
 POLICY
